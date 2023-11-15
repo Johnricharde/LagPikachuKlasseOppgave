f05f7a09f41c6dd240d79070ff2b7c63bac1185b
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-15 17:32:38 +0100

    Add project files.

---
+++ LagPikachuKlasseOppgave/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace LagPikachuKlasseOppgave
+{
+    class Pikachu
+    {
+        public string name = "Pikachu";
+        public int Lvl = 10;
+        public int Hp = 25;
+    }
+}
---
+++ LagPikachuKlasseOppgave/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LagPikachuKlasseOppgave
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pikachu myPokemon = new Pikachu();
+            Console.WriteLine($"Name: {myPokemon.name}\nLevel: {myPokemon.Lvl}\nHealth: {myPokemon.Hp}");
+        }
+    }
+}
